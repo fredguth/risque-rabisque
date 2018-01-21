e29ec6bb79653605d1b8f682850514b3634a0a4e
Fred Guth <fredguth@fredguth.com>
2018-01-21 21:59:39 -0200

    initial

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
+const merge = require('broccoli-merge-trees');
+const funnel = require('broccoli-funnel');
+const path = require ('path');
 
 module.exports = function(defaults) {
+  let tachyonsPath = path.dirname(require.resolve('tachyons'));
+  let tachyons = funnel(tachyonsPath, {files:['tachyons.css']})
+  let styles = merge ([tachyons, 'src/ui/styles']);
   let app = new GlimmerApp(defaults, {
-    // Add options here
+    trees: { styles }
   });
 
-  return app.toTree();
+   return app.toTree();
 };
--- src/ui/components/PaintApp/component.ts
+++ src/ui/components/PaintApp/component.ts
@@ -1,5 +1,59 @@
-import Component from '@glimmer/component';
-
-export default class PaintApp extends Component {
-
+import Component, { tracked } from '@glimmer/component';
+export default class extends Component {
+  mouseDown(event) {
+     console.log(`${event.type}`);
+  }
+  mouseMove(event) {
+    console.log(`${event.type}`);
+  }
+  mouseUp(event) {
+    console.log(`${event.type}`);
+  }
 }
+// import Component, { tracked } from '@glimmer/component';
+
+// export interface PaintState {
+//   canvas: HTMLCanvasElement;
+//   ctx: CanvasRenderingContext2D;
+// }
+
+// export default class PaintApp extends Component {
+//   @tracked state: PaintState;
+//   reset: PaintState; // initial state
+
+//   didInsertElement() {
+//     let canvas = <HTMLCanvasElement> (this.bounds.firstNode as HTMLElement).querySelector('canvas');
+//     let ctx = canvas.getContext('2d');
+//     canvas.addEventListener('mousedown', this.mouseDown);
+//     canvas.addEventListener('mouseup', this.mouseUp);
+//     canvas.addEventListener('mousemove', this.mouseMove);
+//     this.reset = {
+//       ...this.reset,
+//       canvas,
+//       ctx
+//     }
+//   }
+//   willDestroy(){
+//     // this.reset.canvas.removeEventListener('mousedown', this.mouseDown);
+//     // this.reset.canvas.removeEventListener('mouseup', this.mouseUp);
+//     // this.reset.canvas.removeEventListener('mousemove', this.mouseMove);
+//   }
+//   mouseDown(event) {
+//      console.log(`${event.type}`);
+//      // update(this.state, {type: 'mousedown'})
+//   }
+//   mouseMove(event) {
+//     //console.log(`move:${event.offsetX}, ${event.offsetY}`);
+//     console.log(`${event.type}`);
+//   }
+//   mouseUp(event) {
+//     console.log(`${event.type}`);
+//   }
+//   update(state, cmd) {
+//     let newState:PaintState = this.state | this.reset;
+
+//     return newState;
+//   }
+
+
+// }
