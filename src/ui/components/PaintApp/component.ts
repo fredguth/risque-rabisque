9ea1bf9edc04328aaf364297899f3660a3d2b688
Fred Guth <fredguth@fredguth.com>
2018-01-22 01:02:24 -0200

    working with brush

--- src/ui/components/PaintApp/component.ts
+++ src/ui/components/PaintApp/component.ts
@@ -1,10 +1,12 @@
 
 import Component, { tracked } from '@glimmer/component';
+import { distanceBetween2Points, angleBetween2Points} from '../../../utils/trig';
 
 export interface PaintState {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
   position: Object;
+  brush: HTMLImageElement;
   brushOn: Boolean;
 }
 
@@ -15,14 +17,18 @@ export default class PaintApp extends Component {
   didInsertElement() {
     let canvas = <HTMLCanvasElement> (this.bounds.firstNode as HTMLElement).querySelector('canvas');
     let ctx = canvas.getContext('2d');
+    let brush = new Image();
+    brush.src = 'brush_ps16.png';
     let position = {x:0, y:0};
     this.reset = {
       ...this.reset,
       canvas,
       ctx,
       position,
+      brush,
       brushOn: false
     };
+
     this.state = this.reset;
   }
   mouseAction(event) {
@@ -36,10 +42,10 @@ export default class PaintApp extends Component {
 
   update(state, cmd) {
     let newState:PaintState = state;
-    let {ctx, position} = state;
+    let { ctx, position, brush } = state;
     ctx.strokeStyle = "#df4b26";
-    ctx.lineJoin = "round";
-    ctx.lineWidth = 5;
+    ctx.lineJoin = ctx.lineCap = 'round';
+    ctx.lineWidth = 10;
     ctx.moveTo(position.x, position.y);
     newState = {
          ...newState,
@@ -63,9 +69,21 @@ export default class PaintApp extends Component {
         break;
       }
       case 'mousemove': {
+        // http://www.tricedesigns.com/2012/01/04/sketching-with-html5-canvas-and-brush-images/
+        //
         if (state.brushOn) {
-          ctx.lineTo(cmd.payload.x, cmd.payload.y);
-          ctx.stroke()
+          // ctx.lineTo(cmd.payload.x, cmd.payload.y);
+          // ctx.stroke();
+          let halfBrushW = brush.width/2;
+          let halfBrushH = brush.height/2;
+          let distance = parseInt(distanceBetween2Points(position, cmd.payload));
+          let angle = angleBetween2Points(position, cmd.payload);
+          let x, y, z:number = -1;
+          for (z=0; (z<=distance||z==0); z++) {
+            x = position.x + (Math.sin(angle) * z) - halfBrushW;
+            y = position.y + (Math.cos(angle) * z) - halfBrushH;
+            ctx.drawImage(brush, x, y);
+          }
           newState = {
             ...newState,
             ctx
