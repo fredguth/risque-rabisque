e0ce89096d07c7bcebec31d33eb5fa004a404943
Fred Guth <fredguth@fredguth.com>
2018-01-23 14:52:19 -0200

    throttling and drawing line

--- src/ui/components/PaintApp/component.ts
+++ src/ui/components/PaintApp/component.ts
@@ -1,14 +1,15 @@
 
 import Component, { tracked } from '@glimmer/component';
 import { distanceBetween2Points, middlePointBetween, angleBetween2Points} from '../../../utils/trig';
-import { throttle } from '../../../utils/run';
 
 export interface PaintState {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
+  undoCtx: CanvasRenderingContext2D;
   position: Object;
   brush: HTMLImageElement;
   brushOn: Boolean;
+  linePoints: Array;
 }
 
 export default class PaintApp extends Component {
@@ -18,6 +19,7 @@ export default class PaintApp extends Component {
   didInsertElement() {
     let canvas = <HTMLCanvasElement> (this.bounds.firstNode as HTMLElement).querySelector('canvas');
     let ctx = canvas.getContext('2d');
+    let undoCtx = ctx;
     let background = new Image();
     // background.src = "https://mags.clubeami.com.br/357/files/assets/common/page-html5-substrates/page0003.jpg";
     // background.onload = () => {
@@ -32,9 +34,11 @@ export default class PaintApp extends Component {
       ...this.reset,
       canvas,
       ctx,
+      undoCtx,
       position,
       brush,
-      brushOn: false
+      brushOn: false,
+      linePoints:[]
     };
 
     this.state = this.reset;
@@ -44,77 +48,113 @@ export default class PaintApp extends Component {
       type: event.type,
       payload: {x:event.offsetX, y:event.offsetY}
     }
-    // console.log(cmd);
+    //console.log(`fn: debounce = ${debounce}`);
     this.update(this.state, cmd);
+  }
+
+  drawLine(ctx, points ){
+
+    // references:
+    // http://perfectionkills.com/exploring-canvas-drawing-techniques/
+    // http://www.tricedesigns.com/2012/01/04/sketching-with-html5-canvas-and-brush-images/
+    //
+    // simple line:
+    // ctx.lineTo(cmd.payload.x, cmd.payload.y);
+    // ctx.stroke();
+    //
+    // brush:
+    // let halfBrushW = brush.width/2;
+    // let halfBrushH = brush.height/2;
+    // let distance = parseInt(distanceBetween2Points(position, cmd.payload));
+    // let angle = angleBetween2Points(position, cmd.payload);
+    // let x, y, z:number = -1;
+    // for (z=0; (z<=distance||z==0); z++) {
+    //   x = position.x + (Math.sin(angle) * z) - halfBrushW;
+    //   y = position.y + (Math.cos(angle) * z) - halfBrushH;
+    //   ctx.drawImage(brush, x, y);
+    // }
+    //
+    // qudratic curve:
+
+    // let distance = parseInt(distanceBetween2Points(p1, p2));
+    // let angle = angleBetween2Points(p1, p2);
+    // let midPoint = {
+    //   x: p1.x + (Math.sin(angle) * distance),
+    //   y: p1.y + (Math.cos(angle) * distance)
+    // };
+    //
+    ctx.moveTo(points[0].x, points[0].y);
+    console.log('moveTo:${points[0].x}, ${points[0].y}');
+    let midPoint;
+    let i;
+
+    for (i=1; i < points.length - 2; i++) {
+      if (points[i+1]) {
+        midPoint = {
+        x: (points[i].x + points[i+1].x) / 2,
+        y: (points[i].y + points[i+1].y) / 2,
+      }
+      ctx.quadraticCurveTo(points[i].x, points[i].y, midPoint.x, midPoint.y);
+      }
+    }
+    points[i+1] && ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+    ctx.stroke();
+    return ctx;
   }
 
   update(state, cmd) {
     let newState:PaintState = state;
-    let { ctx, position, brush } = state;
-    ctx.strokeStyle = "#df4b26";
-    ctx.lineJoin = ctx.lineCap = 'round';
-    ctx.lineWidth = 10;
+    let { ctx, position, brush, linePoints, undoCtx } = state;
+    ctx);
+
     ctx.moveTo(position.x, position.y);
     newState = {
-         ...newState,
-         ctx,
-         position: cmd.payload
+      ...newState,
+      ctx
     }
 
     switch (cmd.type) {
       case 'mousedown': {
+
+        undoCtx = ctx;
+        ctx.strokeStyle = "#df4b26";
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = 10;
+        ctx.beginPath();
         newState = {
          ...newState,
-         brushOn: true
+         ctx,
+         brushOn: true,
+         position:cmd.payload,
+         linePoints: [cmd.payload],
+         undoCtx
         }
+
         break;
       }
       case 'mouseup': {
+        undoCtx = ctx;
         newState = {
          ...newState,
-         brushOn: false
+         brushOn: false,
+         position:cmd.payload,
+         undoCtx
         }
         break;
       }
       case 'mousemove': {
-        //if (state.brushOn) {
-        if (true) {
-          throttle(() => {
-            // references:
-            // http://perfectionkills.com/exploring-canvas-drawing-techniques/
-            // http://www.tricedesigns.com/2012/01/04/sketching-with-html5-canvas-and-brush-images/
-            //
-            // simple line:
-            // ctx.lineTo(cmd.payload.x, cmd.payload.y);
-            // ctx.stroke();
-            //
-            // brush:
-            // let halfBrushW = brush.width/2;
-            // let halfBrushH = brush.height/2;
-            // let distance = parseInt(distanceBetween2Points(position, cmd.payload));
-            // let angle = angleBetween2Points(position, cmd.payload);
-            // let x, y, z:number = -1;
-            // for (z=0; (z<=distance||z==0); z++) {
-            //   x = position.x + (Math.sin(angle) * z) - halfBrushW;
-            //   y = position.y + (Math.cos(angle) * z) - halfBrushH;
-            //   ctx.drawImage(brush, x, y);
-            // }
-            //
-            // qudratic curve:
-            // let distance = parseInt(distanceBetween2Points(position, cmd.payload));
-            // let angle = angleBetween2Points(position, cmd.payload);
-            // let midPoint = {
-            //   x: position.x + (Math.sin(angle) * distance),
-            //   y: position.y + (Math.cos(angle) * distance)
-            // };
-            // ctx.quadraticCurveTo(midPoint.x, midPoint.y, cmd.payload.x, cmd.payload.y);
-            // ctx.stroke();
-            // newState = {
-            //   ...newState,
-            //   ctx
-            // }
-            console.log((new Date).toISOString());
-           });
+
+        let fixed = (cmd.payload.x == position.x && cmd.payload.y == position.y);
+
+        if (state.brushOn && !fixed) {
+
+          linePoints.push(cmd.payload);
+
+          newState = {
+            ...newState,
+            ctx: this.drawLine(undoCtx, linePoints)
+          }
 
         }
         break
--- src/utils/run.ts
+++ src/utils/run.ts
@@ -1,26 +1,37 @@
-export const throttle = (fn) => {
-  fn.apply(this, arguments);
+export const throttle = function (fn, period, opts) {
+  opts = opts || {};
+  var leading = opts.leading === false ? false : true; // defaults to true
+  var trailing = !!opts.trailing; // defaults to false
+  var args, cached;
+  var timeout = null, prev = 0;
+
+
+  return function() {
+    var now = Date.now();
+    if (!leading && !prev) prev = now;
+    var timeLeft = period - (now - prev);
+    args = [];
+    for (var i = 0; i < arguments.length; i++) args.push(arguments[i]); // faster slice
+    if (timeLeft <= 0 || timeLeft > period) {
+      clear();
+      prev = now;
+      cached = fn.apply(this, args);
+      if (!timeout) args = null;
+    } else if (!timeout && trailing) {
+      timeout = setTimeout(deferred.bind(this), timeLeft);
+    }
+    return cached;
+  };
+
+  function clear() {
+    if (timeout) clearTimeout(timeout);
+    timeout = null;
+  }
+
+  function deferred() {
+    prev = leading ? Date.now() : 0;
+    timeout = null;
+    cached = fn.apply(this, args);
+    if (!timeout) args = null;
+  }
 }
-// export const throttle = (fn, limit) => {
-
-//     fn.apply(this, arguments);
-
-//   let inThrottle:Boolean = false;
-//   if (!inThrottle) {
-//     fn.apply(this, arguments);
-//     inThrottle = true;
-//     setTimeout(()=> inThrottle = false, limit);
-//   }
-//     inThrottle = true;
-//     setTimeout(()=> inThrottle = false, limit);
-//   }
-//   return function() {
-//     console.log('throttle');
-//     // if (!inThrottle) {
-//     //   console.log(`this:${this}, args:${arguments}`);
-//     //   fn.apply(this, arguments)
-//     //   inThrottle = true
-//     //   setTimeout(() => inThrottle = false, limit)
-//     // }
-//   }
-// }
